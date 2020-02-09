458e4df7fa5305375f4d9c3bac85a7689086dbf1
Nick <nshupinski@gmail.com>
2020-02-08 23:43:10 -0500

    Instanciated a button variable and TextView variables and also checked if the editTexts were empty

--- app/src/main/java/edu/temple/signupform1/FormActivity.java
+++ app/src/main/java/edu/temple/signupform1/FormActivity.java
@@ -7,19 +7,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
-
-
+    EditText editTextName;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    EditText editTextConfirmPassword;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = (EditText) findViewById(R.id.editTextConfirmPassword);
+        btnSave = (Button) findViewById(R.id.btnSave);
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        
+                if (editTextName.getText().toString() == "") {
+                    Toast.makeText(getApplicationContext(),"Please enter your name", Toast.LENGTH_LONG).show();
+                }
+                if (editTextEmail.getText().toString() == "") {
+                    Toast.makeText(getApplicationContext(),"Please enter your email", Toast.LENGTH_LONG).show();
+                }
+                if (editTextPassword.getText().toString() == "") {
+                    Toast.makeText(getApplicationContext(),"Please enter your password", Toast.LENGTH_LONG).show();
+                }
+                if (editTextConfirmPassword.getText().toString() == "") {
+                    Toast.makeText(getApplicationContext(),"Please confirm your password", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
