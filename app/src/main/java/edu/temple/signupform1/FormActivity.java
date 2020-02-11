a4b2df43d3380f6ec283234598dc6f5daa92a5b7
Nick <nshupinski@gmail.com>
2020-02-11 00:00:25 -0500

    A way to check if passwords matched and also that every input was filled in was implemented as well as a toast welcoming the user to the app if all is done correctly. COMPLETE

--- app/src/main/java/edu/temple/signupform1/FormActivity.java
+++ app/src/main/java/edu/temple/signupform1/FormActivity.java
@@ -17,6 +17,10 @@ public class FormActivity extends AppCompatActivity {
     EditText editTextConfirmPassword;
     Button btnSave;
 
+    private boolean allTrue = false;
+
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,23 +32,50 @@ public class FormActivity extends AppCompatActivity {
         editTextConfirmPassword = (EditText) findViewById(R.id.editTextConfirmPassword);
         btnSave = (Button) findViewById(R.id.btnSave);
 
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (editTextName.getText().toString() == "") {
-                    Toast.makeText(getApplicationContext(),"Please enter your name", Toast.LENGTH_LONG).show();
+                boolean nameTrue = true;
+                boolean eTrue = true;
+                boolean passTrue = true;
+                boolean passConfTrue = true;
+                boolean passMatch = true;
+
+                if (editTextName.getText().toString().matches("")) {
+                    Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+                    nameTrue = false;
                 }
-                if (editTextEmail.getText().toString() == "") {
-                    Toast.makeText(getApplicationContext(),"Please enter your email", Toast.LENGTH_LONG).show();
+                else if (editTextEmail.getText().toString().matches("")) {
+                    Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_SHORT).show();
+                    eTrue = false;
                 }
-                if (editTextPassword.getText().toString() == "") {
-                    Toast.makeText(getApplicationContext(),"Please enter your password", Toast.LENGTH_LONG).show();
+                else if (editTextPassword.getText().toString().matches("")) {
+                    Toast.makeText(getApplicationContext(), "Please enter your password", Toast.LENGTH_SHORT).show();
+                    passTrue = false;
                 }
-                if (editTextConfirmPassword.getText().toString() == "") {
-                    Toast.makeText(getApplicationContext(),"Please confirm your password", Toast.LENGTH_LONG).show();
+                else if (editTextConfirmPassword.getText().toString().matches("")) {
+                    Toast.makeText(getApplicationContext(), "Please confirm your password", Toast.LENGTH_SHORT).show();
+                    passConfTrue = false;
                 }
 
+                if ( (!(editTextPassword.getText().toString().matches(""))) && (!(editTextConfirmPassword.getText().toString().matches(""))) ) {
+                    if ( !(editTextPassword.getText().toString().matches(editTextConfirmPassword.getText().toString()) ) ) {
+
+                        Toast.makeText(getApplicationContext(), "Your passwords do not match", Toast.LENGTH_SHORT).show();
+                        passMatch = false;
+                    }
+                }
+
+                if (nameTrue == true && eTrue == true && passTrue == true && passConfTrue == true && passMatch == true)
+                allTrue = true;
+
+
+                if (allTrue == true) {
+                    name = editTextName.getText().toString();
+                    Toast.makeText(getApplicationContext(), "Welcome to the app, " + name + "!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
